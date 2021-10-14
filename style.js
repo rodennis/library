63aafa24719903709af124a9714d5591f1fcdf01
rodennis <rev.rltodd@gmail.com>
2021-10-13 20:56:42 -0500

    fixed to where book input is added into the array on submit

--- style.js
+++ style.js
@@ -2,7 +2,7 @@ const container = document.getElementById('container');
 const add = document.getElementById('add');
 const library = ['noby dick', 'sean connery', 'your mom'];
 
-let book = ( (title, author, pages, hasRead) => {
+function book(title, author, pages, hasRead) {
     this.title = title
     this.author = author
     this.pages = pages
@@ -10,46 +10,29 @@ let book = ( (title, author, pages, hasRead) => {
     this.info = function() {
         console.log(`the ${title} by ${author}, ${pages}, ${hasRead}`)
     }
+}
+
+
+function addBookToLibrary() {
+    add.addEventListener('click', () => {
+        const title = document.getElementById('title').value
+        const author = document.getElementById('author').value
+        const pages = document.getElementById('pages').value
+        const hasRead = document.getElementById('hasRead').value
+        
+        userBook = new book(title, author, pages, hasRead)
+            if (title === '' || author === '' || pages === '' || hasRead === '')
+                return alert('Please fill out the entire form!')
+            
+            else {
+                library.push(`Title: ${userBook.title}, Author: ${userBook.author}, Pages: ${userBook.pages}, Read: ${userBook.hasRead}`)
+        
+                const title = document.getElementById('title').value = ''
+                const author = document.getElementById('author').value = ''
+                const pages = document.getElementById('pages').value = ''
+                const hasRead = document.getElementById('hasRead').value = ''
+            }
 })
+}
 
-add.addEventListener('click', () => {
-    const title = document.getElementById('title').value
-    const author = document.getElementById('author').value
-    const pages = document.getElementById('pages').value
-    const hasRead = document.getElementById('hasRead').value
-
-            const newDiv = document.createElement('div');
-            const userTitle = document.createElement('p')
-            userTitle.setAttribute('id', 'userTitle')
-            const userAuthor = document.createElement('p')
-            userAuthor.setAttribute('id', 'userAuthor')
-            const userPages = document.createElement('p')
-            userPages.setAttribute('id', 'userPages')
-            const userRead = document.createElement('p')
-            userRead.setAttribute('id', 'userRead')
-            newDiv.appendChild(userTitle)
-            newDiv.appendChild(userAuthor)
-            newDiv.appendChild(userPages)
-            newDiv.appendChild(userRead)
-            container.appendChild(newDiv);
-
-            library.push(`${title} by: ${author}, ${pages} pages, Read: ${hasRead}`)
-
-            const postBook = ( () => {
-                document.getElementById('userTitle').innerHTML = title
-                document.getElementById('userAuthor').innerHTML = author
-                document.getElementById('userPages').innerHTML = pages
-                document.getElementById('userRead').innerHTML = hasRead
-            })  
-
-            postBook()
-
-}) 
-
-let displayBooks = ( () => {
-    for (i = 0; i < library.length; i++) {
-        console.log(library[i])
-    }
-})
-
-displayBooks()
+addBookToLibrary()
