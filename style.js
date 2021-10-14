5878e0bc017cbf556a249923981b49860c12ea31
rodennis <rev.rltodd@gmail.com>
2021-10-14 15:06:19 -0500

    add div and p element for each loop through the array

--- style.js
+++ style.js
@@ -15,24 +15,36 @@ function book(title, author, pages, hasRead) {
 
 function addBookToLibrary() {
     add.addEventListener('click', () => {
-        const title = document.getElementById('title').value
-        const author = document.getElementById('author').value
-        const pages = document.getElementById('pages').value
-        const hasRead = document.getElementById('hasRead').value
+        const title = document.getElementById('title').value;
+        const author = document.getElementById('author').value;
+        const pages = document.getElementById('pages').value;
+        const hasRead = document.getElementById('hasRead').value;
         
         userBook = new book(title, author, pages, hasRead)
             if (title === '' || author === '' || pages === '' || hasRead === '')
-                return alert('Please fill out the entire form!')
+                return alert('Please fill out the entire form!');
             
             else {
-                library.push(`Title: ${userBook.title}, Author: ${userBook.author}, Pages: ${userBook.pages}, Read: ${userBook.hasRead}`)
+                library.push(`Title: ${userBook.title}, Author: ${userBook.author}, Pages: ${userBook.pages}, Read: ${userBook.hasRead}`);
         
-                const title = document.getElementById('title').value = ''
-                const author = document.getElementById('author').value = ''
-                const pages = document.getElementById('pages').value = ''
-                const hasRead = document.getElementById('hasRead').value = ''
+                const title = document.getElementById('title').value = '';
+                const author = document.getElementById('author').value = '';
+                const pages = document.getElementById('pages').value = '';
+                const hasRead = document.getElementById('hasRead').value = '';
             }
 })
 }
 
 addBookToLibrary()
+
+let displayBooks = ( () => {
+    for (i = 0; i < library.length; i++) {
+        const newDiv = document.createElement('div');
+        container.appendChild(newDiv);
+        const PostBook = document.createElement('p')
+        newDiv.appendChild(PostBook);
+        console.log(library[i])
+    }
+})
+
+displayBooks()
